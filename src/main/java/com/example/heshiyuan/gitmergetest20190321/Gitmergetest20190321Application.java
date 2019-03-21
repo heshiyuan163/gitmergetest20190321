cf5a83447e225c492d6f670a59fc0691be274416
heshiyuan163 <15801636814@163.com>
2019-03-21 15:05:10 +0800

    init empty springboot project

---
+++ src/main/java/com/example/heshiyuan/gitmergetest20190321/Gitmergetest20190321Application.java
@@ -0,0 +1,13 @@
+package com.example.heshiyuan.gitmergetest20190321;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gitmergetest20190321Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gitmergetest20190321Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/heshiyuan/gitmergetest20190321/Gitmergetest20190321ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.heshiyuan.gitmergetest20190321;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Gitmergetest20190321ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
